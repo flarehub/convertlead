51c261f8200674c73ff82e6e5922480e38ddcdb7
Dmitri Russu <dmitri.russu@gmail.com>
2019-01-27 16:38:37 +0200

    have improved login

--- backend/app/Http/Controllers/Auth/ApiLoginController.php
+++ backend/app/Http/Controllers/Auth/ApiLoginController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Lang;
+use Laravel\Passport\Passport;
 
 class ApiLoginController extends Controller {
-    use AuthenticatesUsers;
     
-    protected function authenticated(\Illuminate\Http\Request $request, $user)
+    protected function login(\Illuminate\Http\Request $request)
     {
         $request->request->add([
             'grant_type' => 'password',
@@ -18,51 +19,12 @@ class ApiLoginController extends Controller {
             'client_secret' => env('APP_CLIENT_SECRET'),
             'username' => $request->get('email'),
         ]);
-        
         $request->request->add(['scope' => 'manage-leads']);
-        
         $tokenRequest = Request::create(
             '/oauth/token',
             'post',
             $request->all()
         );
-        
         return \Route::dispatch($tokenRequest);
-    }
-    
-    /**
-     * Send the response after the user was authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    protected function sendLoginResponse(Request $request)
-    {
-        $this->clearLoginAttempts($request);
-        
-        return $this->authenticated($request, $this->guard()->user())
-            ?: response()->json([
-                "status"=>"error",
-                "message"=> "Something went wrong!"], 401);
-        
-    }
-    
-    /**
-     * Get the failed login response instance.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    protected function sendFailedLoginResponse(Request $request)
-    {
-        return response()->json([
-            "status"=>"error",
-            "message"=>"Autentication Error",
-            "data"=>[
-                "errors"=>[
-                    $this->username() => Lang::get('auth.failed'),
-                ]
-            ]
-        ]);
     }
 }
--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -2,12 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Laravel\Passport\AuthCode;
-use Laravel\Passport\Client;
 use Laravel\Passport\Passport;
-use Laravel\Passport\PersonalAccessClient;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -32,9 +28,6 @@ class AuthServiceProvider extends ServiceProvider
         Passport::routes();
         Passport::tokensExpireIn(now()->addDays(15));
         Passport::refreshTokensExpireIn(now()->addDays(30));
-        Passport::useClientModel(Client::class);
-        Passport::useAuthCodeModel(AuthCode::class);
-        Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
     
         Passport::tokensCan([
             'manage-leads' => 'Manage leads',
