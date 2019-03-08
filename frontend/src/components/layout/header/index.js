5b9e6a46bb5227b28fcf298ee9ca9dfb562e1c29
Dmitri Russu <dmitri.russu@gmail.com>
2019-03-08 22:08:54 +0200

    implemented user profile

--- frontend/src/components/layout/header/index.js
+++ frontend/src/components/layout/header/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Header, Image } from 'semantic-ui-react';
+import { compose, lifecycle } from 'recompose';
 import styles from './index.scss';
 import { Breadcrumb } from 'components';
 import { ProfileContainer } from '@containers';
 
-const Header = ({ profile }) => (
+const LayoutHeader = ({ profile }) => (
   <div className={styles.Header}>
     <Grid columns={2} stackable>
       <Grid.Row verticalAlign="middle">
         <Grid.Column>
           <Breadcrumb />
         </Grid.Column>
-        <Grid.Column>
-          <label>{ profile.name }</label>
+        <Grid.Column textAlign='right'>
+          <Header as='h2'>
+            { profile.name }
+          </Header>
         </Grid.Column>
       </Grid.Row>
     </Grid>
   </div>
 );
 
-export default ProfileContainer(Header);
+export default compose(ProfileContainer)(LayoutHeader);
