17dcf9c3a18bd1c4c4fcd8a63a593d519b8aac15
Dmitri Russu <dmitri.russu@gmail.com>
2019-03-16 16:02:56 +0200

    done fix for get agency agent graph

--- frontend/src/@containers/agents/api.js
+++ frontend/src/@containers/agents/api.js
@@ -1,6 +1,6 @@
-import { api } from "@services";
+import { api, Auth } from "@services";
 
-export const companyAgentLeadGraph = (agentId, filters) => api.get(`/v1/company/agents/${agentId}/graph/${filters.graphType}`, {
+export const companyAgentLeadGraph = (agentId, filters) => api.get(`/v1/${Auth.role}/agents/${agentId}/graph/${filters.graphType}`, {
   params: {
     ...filters
   }
