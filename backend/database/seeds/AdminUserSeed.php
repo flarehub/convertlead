333f0f923043d976a225f46c8162a4ce978d4207
Dmitri Russu <dmitri.russu@gmail.com>
2019-07-22 20:41:39 +0300

    I have removed the commented send email

---
+++ backend/database/seeds/AdminUserSeed.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminUserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::create([
+            'email' => 'alex.hypetutor@gmail.com',
+            'name' => 'Dmitri Russu',
+            'password' => bcrypt('testtest'),
+            'role' => \App\Models\User::$ROLE_ADMIN,
+        ]);
+    }
+}
