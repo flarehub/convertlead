14319d9ec43e955fb497920c3aec1c37efaeb910
Alex <costan.alex88@yahoo.ro>
2019-11-06 22:18:17 +0200

    email template changes

--- backend/resources/views/emails/account-closed.blade.php
+++ backend/resources/views/emails/account-closed.blade.php
@@ -81,8 +81,8 @@
 
 </style>
 
-<div class="emailbody">
-	<div class="toplogocont"><img src="http://convertlead.com/wp-content/bloglogo.png"></img></div>
+<div class="emailbody" style="width:100%; height:100%; display:block; margin:0 auto; background:#f1f4f5; padding: 60px 0; ">
+	<div class="toplogocont" style="text-align:center; display:block; width:100%; "><img src="http://convertlead.com/wp-content/bloglogo.png" style="width: 120px; margin: 10px auto !important; text-align: center; padding: 15px; display:block;"></img></div>
 	<div class="bigcont" style="max-width: 380px; background:#fff; border: 1px dashed #b3b3b3; padding: 55px; margin: 0 auto; border-radius: 6px; color:#354771;">>
 		<p style="font-size: 18px; font-family: Arial, sans-serif; font-weight: 400; line-height: 26px; ">Hey, </br></br> This is a confirmation message to let you know your account was closed : </p>
 		<h1>Account {{$user->email}} closed</h1>
