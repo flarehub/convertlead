e6e9d743b388a764f7a391ec25a1c6ed5a2b638c
Alex <costan.alex88@yahoo.ro>
2019-11-06 22:11:54 +0200

    email template changes

--- backend/resources/views/emails/agent-welcome.blade.php
+++ backend/resources/views/emails/agent-welcome.blade.php
@@ -99,23 +99,25 @@
 
 </style>
 
-<div class="emailbody">
-	<div class="toplogocont"><img src="http://convertlead.com/wp-content/bloglogo.png"></img></div>
-	<div class="bigcont">
-		<p>Hey {{ $agent->name  }}, </br></br> {{ $company->name  }} just added you to their ConvertLead team. Welcome ! </p></br>
+
+<div class="emailbody" style="width:100%; height:100%; display:block; margin:0 auto; background:#f1f4f5; padding: 60px 0; ">
+	<div class="toplogocont" style="text-align:center; display:block; width:100%; "><img src="http://convertlead.com/wp-content/bloglogo.png" style="width: 120px; margin: 10px auto !important; text-align: center; padding: 15px; display:block;"></img></div>
+	<div class="bigcont" style=" max-width: 380px; background:#fff; border: 1px dashed #b3b3b3; padding: 55px; margin: 0 auto; border-radius: 6px; color:#354771;">
+		<p style="font-size: 18px; font-family: Arial, sans-serif; font-weight: 400; line-height: 26px; ">Hey {{ $agent->name  }}, </br></br> {{ $company->name  }} just added you to their ConvertLead team. Welcome ! </p></br>
 
 
-		<p><p>Please take a second to download our mobile. Once you do that, you'll be able to login to your account using the following details: </p>
+		<p style="font-size: 18px; font-family: Arial, sans-serif; font-weight: 400; line-height: 26px; "> Please take a second to download our mobile. Once you do that, you'll be able to login to your account using the following details: </p>
 
-		<h1>User: {{$agent->email}}</h1>
-		<h1>Password: {{$password}}</h1>
+		<h1 style=" font-size: 17px; font-family: Arial, sans-serif; font-weight: 600; color:#354771; ">User: {{$agent->email}}</h1>
+		<h1 style=" font-size: 17px; font-family: Arial, sans-serif; font-weight: 600; color:#354771; ">Password: {{$password}}</h1>
 
 
-		<a href="https://convertlead.com/downloads">Visit app download page</a>
-		<p class="smallnote">Didn't sign up for ConvertLead ? <a href="https://support.convertlead.com">Let us know</a></p>
+		<a style="display: block; background:#005ff8; color: #fff; font-size: 16px; font-weight: bold; font-family: Arial,sans-serif; margin: 30px auto; padding: 12px 30px; border-radius: 3px; text-decoration: none; text-align: center;
+		white-space: nowrap; box-sizing: border-box; " href="https://convertlead.com/downloads">Visit app download page</a>
+		<p class="smallnote" style="font-size:12px ; text-align:center ; ">Didn't sign up for ConvertLead ? <a href="https://support.convertlead.com">Let us know</a></p>
 
 	</div>
-	<div class="emailfot">
+	<div class="emailfot" style="max-width: 450px; text-align: left; font-size: 12px; margin: 20px auto; color:#797979; ">
 		This is an automated message. Please do not reply. You are receiving this email because you have an agent account on ConvertLead. If you wish to unsubscribe, please visit your ConvertLead settings.
 
 		All rights reserved, ConvertLead © 2019
