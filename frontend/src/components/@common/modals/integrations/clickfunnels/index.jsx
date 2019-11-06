433713a73d68b63a51437028acd9369c4f32fd3a
Dmitri Russu <dmitri.russu@gmail.com>
2019-11-06 22:11:14 +0200

    1: app rebuild

--- frontend/src/components/@common/modals/integrations/clickfunnels/index.jsx
+++ frontend/src/components/@common/modals/integrations/clickfunnels/index.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
-import { Button, Modal } from 'semantic-ui-react'
+import {Button, Form, Input, Modal} from 'semantic-ui-react'
 import './index.scss';
 import WebHook from "components/@common/forms/integrations/webhook";
+import CopyText from "react-copy-text";
+import ReactJson from "react-json-view";
+import RequestLeadSchema from "../../../forms/integrations/webhook/schemas/reques-lead";
+import ResponseLeadSchema from "../../../forms/integrations/webhook/schemas/response-lead";
 
 class ClickFunnelsIntegrationModal extends Component {
   state = {
@@ -9,12 +13,27 @@ class ClickFunnelsIntegrationModal extends Component {
   };
 
   render() {
-    const { campaignLink }  = this.props;
     return (
       <Modal className='ApiIntegration' open={this.props.open} onClose={this.props.onClose}>
         <Modal.Header>ClickFunnels API Integration</Modal.Header>
         <Modal.Content>
-          <WebHook campaignLink={campaignLink}/>
+          <Form>
+            <Form.Field>
+              <h2>Campaign Link</h2>
+              <a href='https://convertlead.com/convertlead-overview/convert-lead-clickfunnels-integration/' target='__blank'>
+                Integration info
+              </a>
+              <CopyText text={this.state.value}  />
+              <Input
+                action={{
+                  color: 'teal',
+                  labelPosition: 'right',
+                  icon: 'copy',
+                  content: `${(this.state.copied ? 'Copied' : 'Copy')}`, onClick: this.onCopy }}
+                defaultValue={this.props.campaignLink}
+              />
+            </Form.Field>
+          </Form>
         </Modal.Content>
         <Modal.Actions>
           <Button color='black' onClick={this.props.onClose}>
