ec942876bfe90383ffdffafa42d5c79c20236a34
Dmitri Russu <dmitri.russu@gmail.com>
2020-09-06 21:40:24 +0300

    done fixes for job scheduler, also added subject for email

--- backend/resources/views/emails/lead-notification.blade.php
+++ backend/resources/views/emails/lead-notification.blade.php
@@ -3,5 +3,5 @@
 	<p>
 		{{$message}}
 	</p>
-	<img src="{{route('LeadReplyController@onMailReply', [ 'lead' => $leadId, 'dealAction' => $dealActionId ])}}" width="1" height="1">
+	<img src="{{\route('LeadReplyController@onMailReply', [ 'lead' => $leadId, 'dealAction' => $dealActionId ])}}" width="1" height="1">
 </div>
