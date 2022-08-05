5f2238ac344c6ec6ab1f121a48aa924afb37b0d9
DmitriRussu <dmitrir@iriscrm.com>
2022-08-06 01:07:31 +0300

    added sender

--- backend/app/Services/MailService.php
+++ backend/app/Services/MailService.php
@@ -23,6 +23,7 @@ Class MailService {
         try {
             $mail = Mail::send($template, $params, function (Message $m) use ($email, $subject, $cc, $attachment, $params) {
                 if (isset($params['from_address'])) {
+                    $m->sender(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME', 'No Reply'));
                     $m->from($params['from_address'], $params['from_address_name'] ?? '');
                 }
 
